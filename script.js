29a0bb199dbc2c304ac765551b64cc02bd500d8f
Fabian <fabian.gretsch@gmail.com>
2022-12-29 01:01:23 +0100

    added more details for poke cards

--- script.js
+++ script.js
@@ -4,62 +4,75 @@ const API_URL_POKEMONS = "https://pokeapi.co/api/v2/pokemon/";
 const API_URL_SPECIES = "https://pokeapi.co/api/v2/pokemon-species/"; // to get german names
 const API_URL_TYPES = "https://pokeapi.co/api/v2/type/"; //to get german type names
 
-// const pokemons = [];
-// const germanNames = [];
+// const allPokemons = [];
 
 const pokeCardContainer = document.querySelector(".poke-card-container");
 
 let startPokemon = 1;
-let endPokemon = 20;
+let endPokemon = 151;
 
 async function renderPokemons() {
   pokeCardContainer.innerHTML = "";
   for (let i = startPokemon; i <= endPokemon; i++) {
     const pokemon = await loadPokemon(i);
-    const germanName = await loadGermanName(i);
-    pokeCardContainer.innerHTML += createPokeCardHtml(pokemon, germanName);
+    // const germanName = await loadGermanName(i);
+    pokeCardContainer.innerHTML += createPokeCardHtml(pokemon, i);
+    console.log(i);
   }
 }
 
 async function loadPokemon(id) {
   const response = await fetch(`${API_URL_POKEMONS}${id}`).catch(errorFunction);
   const pokemonJson = await response.json();
-
+  //   allPokemons.push(pokemonJson);
+  //   console.log(typeof allPokemons);
   return pokemonJson;
 }
 
-async function loadGermanName(id) {
-  const response = await fetch(`${API_URL_SPECIES}${id}`).catch(errorFunction);
-  const speciesJson = await response.json();
-  console.log(speciesJson.names[5].name);
-  return speciesJson.names[5].name;
-}
+// async function loadGermanName(id) {
+//   const response = await fetch(`${API_URL_SPECIES}${id}`).catch(errorFunction);
+//   const speciesJson = await response.json();
+//   console.log(speciesJson.names[5].name);
+//   return speciesJson.names[5].name;
+// }
 
 function errorFunction() {
   console.log("Fehler aufgetreten");
 }
 
-function createPokeCardHtml(pokemon, germanName) {
+function createPokeCardHtml(pokemon, i) {
   return `
-        <div class="poke-card grass">
+        <div class="poke-card ${
+          pokemon.types[0].type.name
+        }" id="poke-card-${i}">
             <div class="name-id">
-            <div class="name">${germanName}</div>
+            <div class="name">${pokemon.name}</div>
             <div class="id">#${String(pokemon.id).padStart(3, "0")}</div>
             </div>
             <div class="types-image">
             <div class="types">
-                <div class="type grass">grass</div>
-                <div class="type poison">poison</div>
+                ${createTypesHtml(pokemon)}
             </div>
             <div class="image">
                 <img
-                src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg"
+                src="${pokemon.sprites.other.dream_world.front_default}"
                 alt="Pokemon image"
                 />
             </div>
             </div>
         </div>
     `;
+}
+
+function createTypesHtml(pokemon) {
+  const types = pokemon.types;
+  let typesHtml = "";
+  for (let type of types) {
+    typesHtml += `
+        <div class="type ${type.type.name}">${type.type.name}</div>
+    `;
+  }
+  return typesHtml;
 }
 
 renderPokemons();
